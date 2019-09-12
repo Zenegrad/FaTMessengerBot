5297118cc04ac78822c6b0191243835923daa7c5
Kyle Murphy <murph754@hotmail.com>
2019-09-12 16:52:53 -0400

    Added regex to parse through the file using white space so we not longer need the |'s. Also added the beginning of the assignChannel function to specifically keep the bot in a assigned channel.

--- my_bot.js
+++ my_bot.js
@@ -1,18 +1,49 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+var assignedChannel;
+var regexPattern = new RegExp('(~\S+).(\S+).(.*)');
 
 if(client.guilds)
 client.on('ready', () => {
     console.log("Connected as " + client.user.tag);
     //console.log(client.guilds.tap(roles => console.log(roles.id)))
-})
+});
+
+
+//Calls this function when a message is sent on discord
+client.on('message', (receivedMessage) => {
+  //Switch this to regex so it finds the key words.
+  var messageArguments = receivedMessage.content.split("|");
+
+  // Prevent bot from responding to its own messages
+  if (receivedMessage.author == client.user) {
+    return;
+  }
+  
+  //Checks to make sure the message is sent in the assigned channel.
+  if(receivedMessage.channel.id == assignedChannel || assignedChannel == null){  
+    switch(messageArguments[0]){
+      case "~dm":
+      case "~dm ":
+        dmCommand(receivedMessage);
+        break;
+
+      case "~assignChannel":
+        assignChannel(receivedMessage);
+        break;
+    }
+  }
+});
 
 //Finds the role ID from a mention/ping
 function getIdFromMention(mention){
 
+  //Role parse.
   if(mention.startsWith('<@&') && mention.endsWith('>')){
     mention = mention.slice(3,-1);
-
+    
+    //@here @everyone parse.
     if(mention.startsWith('!')){
       mention = mention.slice(1);
     }
@@ -21,66 +52,65 @@ function getIdFromMention(mention){
   return null;
 }
 
-//Calls this function when a message is sent on discord
-client.on('message', (receivedMessage) => {
-    var messageArguments = receivedMessage.content.split("|");
+//Function that handles ~dm command (sends a DM to all members involved)
+function dmCommand(receivedMessage){
+  
+  var messageArguments = receivedMessage.content.split(regexPattern);
 
-    // Prevent bot from responding to its own messages
-    if (receivedMessage.author == client.user) {
-        return;
-    }
-    
+  //DM Command
+  if(messageArguments.group(0) === "~dm"){
+        
     //makes sure the command has correct number of args
-    if(messageArguments.length === 3){
+    if(len(messageArguments.groups()) === 3){
 
-      //checks for the correct prefix for command
-      if(messageArguments[0] === "~dm" || messageArguments[0] === "~dm "){
-        console.log("Recieved command");
-        //gets the @roles ready for parsing
-        let rolesListed = messageArguments[1].trim().split(" ");
-        
-        //User[] of all the users with the role
-        let membersWithRole;
+      console.log("Recieved command");
+      //gets the @roles ready for parsing
+      let rolesListed = messageArguments.group(1).trim().split(" ");
+      
+      //User[] of all the users with the role
+      let membersWithRole;
 
-        //Doesnt kill the bot if it hits an error.
-        try{
-          //Finds all users with the roles pinged.
-          for(var i = 0; i < rolesListed.length; i++){
-            try{
-              membersWithRole = receivedMessage.guild.roles.get(getIdFromMention(rolesListed[i])).members.map(m=>m.user);
+      //Doesnt kill the bot if it hits an error.
+      try{
+        //Finds all users with the roles pinged.
+        for(var i = 0; i < rolesListed.length; i++){
+          try{
+            membersWithRole = receivedMessage.guild.roles.get(getIdFromMention(rolesListed[i])).members.map(m=>m.user);
 
-            }catch(error){
-              console.error(error);
-            }
+          }catch(error){
+            console.error(error);
           }
-
-          //loops through membersWithRole and sends them a message, if blocked will send a message back in the channel saying who blocked the bot.
-          for(var i = 0; membersWithRole.length > i; i++){
-            let username = membersWithRole[i].username;
-
-            
-            membersWithRole[i].send(messageArguments[2]).catch(error => {
-              if(error.code === 50007){
-                receivedMessage.channel.send(username + " has blocked the bot.");
-              }
-            });
-              
-          }
-        }catch(e){
-          console.error(e)
-          receivedMessage.channel.send("Somethings not right... make sure you used the correct command format! ``~dm | @role | message``");
         }
-        
+
+        //loops through membersWithRole and sends them a message, if blocked will send a message back in the channel saying who blocked the bot.
+        for(var i = 0; membersWithRole.length > i; i++){
+          let username = membersWithRole[i].username;
+
+          
+          membersWithRole[i].send(messageArguments.group(2)).catch(error => {
+            if(error.code === 50007){
+              receivedMessage.channel.send(username + " has blocked the bot.");
+            }
+          });
+            
+        }
+      }catch(e){
+        console.error(e);
+        receivedMessage.channel.send("Somethings not right... make sure you used the correct command format! ``~dm | @role | message``");
       }
+    }
   }
-  else{
-    receivedMessage.channel.send("Incorrect number of arguments. ``~dm | @role | message``");
-  }
-})
+}
+
+function assignChannel(receivedMessage){
+  
+  //Figure out fucking regex properly.
+  receivedMessage.split(" ");
+}
 
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
-const bot_secret_token = ""
+const bot_secret_token = "NjEyMTM5MTI2NjUyOTI4MDE3.XXnX2g.KGkpEwowRHHg4H874feqOQZEQqw";
 
-client.login(bot_secret_token)
+client.login(bot_secret_token);
